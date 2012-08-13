8d1bf56e6cfe7c6ea7a23c82d51ed6183c544d24
Alexander Balashov <divineforest@gmail.com>
2012-08-13 13:23:08 +0400

    Save error to Charge

--- app/controllers/gateway_notifications_controller.rb
+++ app/controllers/gateway_notifications_controller.rb
@@ -4,12 +4,19 @@ class GatewayNotificationsController < ApplicationController
   def create
     @notification = GatewayInputNotification.create!(params: params, gateway: params[:gateway], raw_post: request.raw_post)
 
-    if @notification.complete? && @notification.acknowledge
+    error = if !@notification.complete?
+      "not_completed"
+    elsif !@notification.acknowledge
+      "acknowledge_failed"
+    end
+
+    if error
+      @notification.charge.failed!
+      @notification.charge.update_attribute(:error, error)
+      head :bad_request
+    else
       @notification.charge.approve unless @notification.charge.ok?
       render :text => @notification.success_response
-    else
-      @notification.charge.failed!
-      head :bad_request
     end
   end
 
